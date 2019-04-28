50af864f05462b6944ccf24d9858eaa15732f3f0
小和尚破了什么戒 <775357468@qq.com>
2019-04-28 18:41:39 +0800

    解决puppeteer安装问题

--- 刷流量/juejin.js
+++ 刷流量/juejin.js
@@ -1,18 +1,19 @@
-// const fs = require('fs');
-// const puppeteer = require('puppeteer');
-import fs from 'fs'
-import puppeteer from 'puppeteer'
+ const fs = require('fs');
+ const path = require('path');
+ const puppeteer = require('puppeteer-core'); 
 
 //执行入口
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
+	executablePath: path.resolve('C:/Program Files (x86)/Google/Chrome/Application/chrome.exe')
   });
   const page = await browser.newPage();
   for (let index = 1; index < 50000; index++) {
     await page.waitFor(1000);
-    await page.goto('https://juejin.im/post/5a7c36c3f265da4e82633a0b');
-    await page.goto('https://juejin.im/post/5a324ddf51882552de5e2ab7');
+    await page.goto('https://juejin.im/post/5cc580abf265da037d4fb5ab');
+    await page.goto('https://juejin.im/post/5ca2d47551882543d8590702');
+    await page.goto('https://juejin.im/post/5c08b1826fb9a049a81f1d49'); 
     console.log('第' + index + '次');
   }
   browser.close();
